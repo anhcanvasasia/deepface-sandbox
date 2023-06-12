3ab40531ca703c6f45eb8195b4e929ebafdcf3fd
cnvs_anh <nguyenngoctramanh@canvas-works.asia>
2023-06-12 09:04:40 +0700

    -

--- tests/stream.py
+++ tests/stream.py
@@ -1,8 +1,8 @@
 from deepface import DeepFace
 
-DeepFace.stream("dataset") #opencv
-#DeepFace.stream("dataset", detector_backend = 'opencv')
-#DeepFace.stream("dataset", detector_backend = 'ssd')
-#DeepFace.stream("dataset", detector_backend = 'mtcnn')
-#DeepFace.stream("dataset", detector_backend = 'dlib')
-#DeepFace.stream("dataset", detector_backend = 'retinaface')
+DeepFace.stream(db_path="dataset/canvas-asia-face-img",
+                model_name='ArcFace',
+                detector_backend='retinaface',
+                enable_face_analysis=False,
+                time_threshold=1,
+                frame_threshold=1)
